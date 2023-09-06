4580e595940ed081cc036468f85fa9ba2c49d34e
Matheus Lucca <mat.lualves@gmail.com>
2023-09-06 01:41:46 -0300

    create user entity

---
+++ src/main/java/br/com/matlucca/apitests/domain/User.java
@@ -0,0 +1,20 @@
+package br.com.matlucca.apitests.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+}
