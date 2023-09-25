1d3a0b1054934f439c93c951b430f96fd6ec9802
Matheus Lucca <mat.lualves@gmail.com>
2023-09-25 12:39:41 -0300

    create bean model mapper and return user dto in controller

--- src/main/java/br/com/matlucca/apitests/controllers/UserController.java
+++ src/main/java/br/com/matlucca/apitests/controllers/UserController.java
@@ -1,7 +1,9 @@
 package br.com.matlucca.apitests.controllers;
 
 import br.com.matlucca.apitests.domain.User;
+import br.com.matlucca.apitests.domain.dto.UserDto;
 import br.com.matlucca.apitests.services.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,9 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     @Autowired
+    private ModelMapper mapper;
+
+    @Autowired
     private UserService service;
     @GetMapping(value = "/{id}")
-    public ResponseEntity<User> findById(@PathVariable Integer id){
-        return ResponseEntity.ok().body(service.findById(id));
+    public ResponseEntity<UserDto> findById(@PathVariable Integer id){
+        return ResponseEntity.ok().body(mapper.map(service.findById(id), UserDto.class));
     }
 }
--- src/main/java/br/com/matlucca/apitests/domain/dto/UserDto.java
+++ src/main/java/br/com/matlucca/apitests/domain/dto/UserDto.java
@@ -1,5 +1,6 @@
 package br.com.matlucca.apitests.domain.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Column;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -15,5 +16,7 @@ public class UserDto {
     private String name;
 
     private String email;
+
+    @JsonIgnore
     private String password;
 }
