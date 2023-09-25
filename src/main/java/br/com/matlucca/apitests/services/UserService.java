ac0feb95051fef14301b6a9ee83f0f3abe4ccd54
Matheus Lucca <mat.lualves@gmail.com>
2023-09-25 12:55:27 -0300

    endpoint user findAll

--- src/main/java/br/com/matlucca/apitests/services/UserService.java
+++ src/main/java/br/com/matlucca/apitests/services/UserService.java
@@ -2,7 +2,10 @@ package br.com.matlucca.apitests.services;
 
 import br.com.matlucca.apitests.domain.User;
 
+import java.util.List;
+
 public interface UserService {
 
     User findById(Integer id);
+    List<User> findAll();
 }
