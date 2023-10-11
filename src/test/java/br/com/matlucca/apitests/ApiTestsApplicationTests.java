83a387571423b19bf0c3594b3cb35e4cc134c6be
Matheus Lucca <mat.lualves@gmail.com>
2023-10-11 00:57:54 -0300

    test main

--- src/test/java/br/com/matlucca/apitests/ApiTestsApplicationTests.java
+++ src/test/java/br/com/matlucca/apitests/ApiTestsApplicationTests.java
@@ -1,13 +1,15 @@
 package br.com.matlucca.apitests;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ApiTestsApplicationTests {
 
     @Test
-    void contextLoads() {
+    void main() {
+        ApiTestsApplication.main(new String[] {});
     }
 
 }
